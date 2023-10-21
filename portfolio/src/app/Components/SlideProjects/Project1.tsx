e893492e23afd75024b280d662bc465786fa31b6
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-10-21 17:37:00 +0200

    Adds slide in animation for projects section

---
+++ portfolio/src/app/Components/SlideProjects/Project1.tsx
@@ -0,0 +1,15 @@
+import { useSpring, animated as a } from "@react-spring/web";
+
+export default function Project1({ children, isVisible }: any) {
+  const animatedProps = useSpring({
+    opacity: isVisible ? 1 : 0,
+    x: isVisible ? 0 : 1200,
+    config: { mass: 4, tension: 400, friction: 100 },
+  });
+
+  return (
+    <a.div className="spring-in" style={{ ...animatedProps }}>
+      {children}
+    </a.div>
+  );
+}
--- portfolio/src/app/Layouts/Projects.tsx
+++ portfolio/src/app/Layouts/Projects.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import Tile from "../Components/Tile";
+import React, { useState } from "react";
+import Project1 from "../Components/SlideProjects/Project1";
+import Project2 from "../Components/SlideProjects/Project2";
+import Skills from "@/app/Layouts/Skills";
 
 export default function Projects(): JSX.Element {
-  return (
-    <div className="bg-slate-300 p-8 h-screen flex flex-col items-center">
-      <h2 className="text-2xl">Projects</h2>
-      <p>This is the content of Projects</p>
+  const [startAnimation, setStartAnimation] = useState(false);
 
-      <Tile />
+  return (
+    <div className="relative h-screen flex flex-col overflow-hidden">
+      <h2 className="text-2xl p-8">Projects</h2>
+      <button
+        className="z-10"
+        onClick={() => setStartAnimation(!startAnimation)}
+      >
+        Toggle Animation
+      </button>
+      <Project1 isVisible={startAnimation}>
+        <div className="w-full absolute left-0">
+          <Skills />
+        </div>
+      </Project1>
     </div>
   );
 }
