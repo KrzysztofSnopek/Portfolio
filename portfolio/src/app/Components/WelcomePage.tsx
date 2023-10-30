44ffeb23f35ffe22f5d2a82bf393b1fac1cb09c9
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-10-30 20:20:20 +0100

    Welcome Page fade out animation

--- portfolio/src/app/Components/WelcomePage.tsx
+++ portfolio/src/app/Components/WelcomePage.tsx
@@ -1,13 +1,55 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import AnimatedLetters from "./Hero/AnimatedLetters";
 
 export const WelcomePage = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+
   return (
     <>
-      <div className="h-screen w-full bg-[#158c35] flex flex-col items-center justify-center">
-        WelcomePage
-      </div>
+      <motion.div className="h-screen w-full bg-[#241752] flex flex-col items-center justify-center">
+        <h1 className="text-[#dee758] text-4xl font-bold uppercase">
+          WelcomePage
+        </h1>
+        <motion.ul variants={container} initial="hidden" animate="show">
+          <motion.li variants={item}>new li</motion.li>
+          <motion.li variants={item}>second li</motion.li>
+          <AnimatePresence>
+            {isVisible && (
+              <motion.div
+                className="w-[150px] h-[150px] bg-red-800"
+                onClick={() => setIsVisible(!isVisible)}
+                variants={item}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  duration: 1,
+                  staggerChildren: 1,
+                  ease: "backOut",
+                }}
+              ></motion.div>
+            )}
+          </AnimatePresence>
+        </motion.ul>
+      </motion.div>
     </>
   );
 };
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -12,6 +12,7 @@ import Resume from "./Layouts/Resume";
 import Skills from "./Layouts/Skills";
 import Project1 from "./Components/SlideProjects/Project1";
 import { WelcomePage } from "./Components/WelcomePage";
+import { motion, AnimatePresence, useTransform } from "framer-motion";
 
 const App: React.FC = () => {
   const [activeComponent, setActiveComponent] = useState<string | null>(null);
@@ -25,67 +26,83 @@ const App: React.FC = () => {
     }
   };
 
-  const handleWelcome = () => {
+  const handleHideWelcome = () => {
     setIsWelcoming(false);
   };
 
   return (
     <>
       {isWelcoming ? (
-        <div onClick={handleWelcome}>
-          <WelcomePage />
-        </div>
+        <AnimatePresence>
+          <motion.div
+            onClick={handleHideWelcome}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileTap={{ opacity: 0 }}
+            transition={{ duration: 2, ease: "backOut" }}
+          >
+            <WelcomePage />
+          </motion.div>
+        </AnimatePresence>
       ) : (
-        <div className="flex relative">
-          <div className="w-1/4 h-screen overflow-hidden z-30">
-            <HeroTheme scrollToComponent={scrollToComponent} />
-          </div>
-          <div className="absolute top-1/2 left-[8vw] transform -translate-y-1/2 w-[500px] z-10">
-            <PhotoBlob />
-          </div>
-          <div className="absolute top-1/2 left-[8vw] transform -translate-y-1/2 w-1/3 z-40">
-            <div className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2">
-              <div
-                id="here"
-                className="h-[360px] w-[360px] bg-slate-800 flex justify-center items-center rounded-full"
-              >
-                <div className="h-72 w-72 relative">
-                  <Image
-                    src="/Images/Hero.jpg"
-                    alt="Picture of the author"
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-full"
-                  />
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 2 }}
+            className="flex relative"
+          >
+            <div className="w-1/4 h-screen overflow-hidden z-30">
+              <HeroTheme scrollToComponent={scrollToComponent} />
+            </div>
+            <div className="absolute top-1/2 left-[8vw] transform -translate-y-1/2 w-[500px] z-10">
+              <PhotoBlob />
+            </div>
+            <div className="absolute top-1/2 left-[8vw] transform -translate-y-1/2 w-1/3 z-40">
+              <div className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2">
+                <div
+                  id="here"
+                  className="h-[360px] w-[360px] bg-slate-800 flex justify-center items-center rounded-full"
+                >
+                  <div className="h-72 w-72 relative">
+                    <Image
+                      src="/Images/Hero.jpg"
+                      alt="Picture of the author"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-full"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="w-[10vw] h-screen bg-primary"></div>
-          <div className="w-[65vw] h-screen bg-primary overflow-y-scroll scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-600">
-            <div id="home" className="py-24">
-              <Home />
+            <div className="w-[10vw] h-screen bg-primary"></div>
+            <div className="w-[65vw] h-screen bg-primary overflow-y-scroll scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-600">
+              <div id="home" className="py-24">
+                <Home />
+              </div>
+              <div id="about" className="py-24">
+                <About />
+              </div>
+              <div id="projects" className="py-24">
+                <Projects />
+              </div>
+              <div>
+                <Project1 />
+              </div>
+              <div id="resume" className="py-24">
+                <Resume />
+              </div>
+              <div id="skills" className="py-24">
+                <Skills />
+              </div>
+              <div id="contact" className="py-24">
+                <Contact />
+              </div>
             </div>
-            <div id="about" className="py-24">
-              <About />
-            </div>
-            <div id="projects" className="py-24">
-              <Projects />
-            </div>
-            <div>
-              <Project1 />
-            </div>
-            <div id="resume" className="py-24">
-              <Resume />
-            </div>
-            <div id="skills" className="py-24">
-              <Skills />
-            </div>
-            <div id="contact" className="py-24">
-              <Contact />
-            </div>
-          </div>
-        </div>
+          </motion.div>
+        </AnimatePresence>
       )}
     </>
   );
