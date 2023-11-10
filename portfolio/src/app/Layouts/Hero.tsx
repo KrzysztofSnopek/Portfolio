eb719de7479bf42dfcea88132795f6b459910a9c
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-11-10 19:25:23 +0100

    Fixes bug when cursor was still pointer on the part of inactive section

--- portfolio/src/app/Layouts/Hero.tsx
+++ portfolio/src/app/Layouts/Hero.tsx
@@ -65,14 +65,16 @@ function Hero() {
                   <div
                     className={classNames(
                       "inset-0 opacity-25 duration-300 before:absolute before:bottom-0 before:left-[-546px] before:right-0 before:top-[-148px] before:z-10 before:bg-texture  after:bottom-[28px] after:left-0 after:right-[-434px] after:top-0 after:z-10 after:bg-texture md:absolute md:transition-opacity",
-                      activeItem === index ? "md:opacity-25 " : "md:opacity-0"
+                      activeItem === index
+                        ? "md:opacity-25 cursor-auto"
+                        : "md:opacity-0"
                     )}
                   />
                   <div
                     className={classNames(
                       "left-8 top-8 w-[590px] p-4 transition-[transform,opacity] md:absolute md:p-0",
                       activeItem === index
-                        ? "md:translate-x-0 md:opacity-100"
+                        ? "md:translate-x-0 md:opacity-100 cursor-auto"
                         : "md:translate-x-4 md:opacity-0"
                     )}
                   >
@@ -80,7 +82,6 @@ function Hero() {
                       <div className="absolute inset-0 bg-accent opacity-70 blur"></div>
                       <p className="text-sm font-extrabold uppercase text-secondary md:text-lg relative z-10">
                         {section.title}
-                        #5f5c64
                       </p>
                     </div>
 
@@ -96,8 +97,9 @@ function Hero() {
                   <div
                     className={classNames(
                       "absolute h-[640px] w-[154px] flex flex-col justify-start items-center p-4 text-4xl font-bold z-30 opacity-0",
-                      activeItem !== index &&
-                        "md:group-hover/item:opacity-100 transition-[opacity]"
+                      activeItem === index
+                        ? "cursor-default"
+                        : "md:group-hover/item:opacity-100 transition-[opacity]"
                     )}
                   >
                     {section.icon && (
