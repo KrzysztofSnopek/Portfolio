97b7048c5915e396fb1f4566a3d5c2255e92a9d7
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-11-14 00:37:54 +0100

    Resolves hover bug that was changing the opacity of element when hovered when it should change it only when the icon was hovered

--- portfolio/src/app/Helpers/CopyToClipboardButton.tsx
+++ portfolio/src/app/Helpers/CopyToClipboardButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ClipboardJS from "clipboard";
 import CopyToClipboardIcon from "../Helpers/CopyToClipboardIcon";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
@@ -16,6 +16,8 @@ export default function CopyToClipboardButton({
   link?: string;
   index: number;
 }) {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
   useEffect(() => {
     const clipboard1 = new ClipboardJS(`#copy-button-${index}`);
 
@@ -25,27 +27,36 @@ export default function CopyToClipboardButton({
   }, []);
 
   return (
-    <div className="group flex relative brightness-75 hover:brightness-100">
-      <span className="bg-lightpurpleaccent text-primary px-2 py-1 rounded-lg group-hover:rounded-l-none group-hover:pl-4">
+    <div
+      className="group flex relative brightness-75 hover:brightness-100"
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <span
+        className="bg-lightpurpleaccent text-primary px-2 py-1 rounded-lg group-hover:rounded-l-none group-hover:pl-4"
+        onMouseEnter={() => setIsHovered(true)}
+      >
         <FontAwesomeIcon icon={icon} />
       </span>
-      <span
-        className="group-hover:opacity-100 transition-[opacity] bg-lightpurpleaccent flex px-2 py-1 text-primary font-bold rounded-l-lg absolute right-full opacity-0 mx-auto whitespace-nowrap"
-        id={`copy-target-${index}`}
-      >
-        {text && <p className="px-2 pr-4">{text}</p>}
-        {link && (
-          <Link className="px-2 pr-4" href={link}>
-            {link}
-          </Link>
-        )}
-        <div
-          data-clipboard-target={`#copy-target-${index}`}
-          id={`copy-button-${index}`}
+      {isHovered && (
+        <span
+          className="bg-lightpurpleaccent flex px-2 py-1 text-primary font-bold rounded-l-lg absolute right-full mx-auto whitespace-nowrap"
+          id={`copy-target-${index}`}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          <CopyToClipboardIcon />
-        </div>
-      </span>
+          {text && <p className="px-2 pr-4">{text}</p>}
+          {link && (
+            <Link className="px-2 pr-4" href={link}>
+              {link}
+            </Link>
+          )}
+          <div
+            data-clipboard-target={`#copy-target-${index}`}
+            id={`copy-button-${index}`}
+          >
+            <CopyToClipboardIcon />
+          </div>
+        </span>
+      )}
     </div>
   );
 }
