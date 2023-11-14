acdbe6b64edb6a614dadf3d0a9807c0e4878d5f5
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-11-14 01:16:11 +0100

    Adds animation to socials

--- portfolio/src/app/Helpers/CopyToClipboardButton.tsx
+++ portfolio/src/app/Helpers/CopyToClipboardButton.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ClipboardJS from "clipboard";
 import CopyToClipboardIcon from "../Helpers/CopyToClipboardIcon";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import {
+  motion,
+  AnimatePresence,
+  useTransform,
+  useMotionValue,
+} from "framer-motion";
 
 export default function CopyToClipboardButton({
   icon,
@@ -37,26 +43,41 @@ export default function CopyToClipboardButton({
       >
         <FontAwesomeIcon icon={icon} />
       </span>
-      {isHovered && (
-        <span
-          className="bg-lightpurpleaccent flex px-2 py-1 text-primary font-bold rounded-l-lg absolute right-full mx-auto whitespace-nowrap"
-          id={`copy-target-${index}`}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          {text && <p className="px-2 pr-4">{text}</p>}
-          {link && (
-            <Link className="px-2 pr-4" href={link}>
-              {link}
-            </Link>
-          )}
-          <div
-            data-clipboard-target={`#copy-target-${index}`}
-            id={`copy-button-${index}`}
+      <AnimatePresence>
+        {isHovered && (
+          <motion.span
+            className="bg-lightpurpleaccent flex px-2 py-1 text-primary font-bold rounded-l-lg absolute right-full mx-auto whitespace-nowrap"
+            id={`copy-target-${index}`}
+            onMouseLeave={() => setIsHovered(false)}
+            initial={{
+              transform: "translateX(20px)",
+              opacity: 0,
+            }}
+            animate={{
+              transform: "translateX(0px)",
+              opacity: [0, 0.1, 0.2, 1],
+              transition: { duration: 0.6 },
+            }}
+            exit={{
+              transform: "translateX(20px)",
+              opacity: 0,
+            }}
           >
-            <CopyToClipboardIcon />
-          </div>
-        </span>
-      )}
+            {text && <p className="px-2 pr-4">{text}</p>}
+            {link && (
+              <Link className="px-2 pr-4" href={link}>
+                {link}
+              </Link>
+            )}
+            <div
+              data-clipboard-target={`#copy-target-${index}`}
+              id={`copy-button-${index}`}
+            >
+              <CopyToClipboardIcon />
+            </div>
+          </motion.span>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
