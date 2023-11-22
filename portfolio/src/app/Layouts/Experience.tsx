0cae135cba16614ae94467d2272faf8354ec23b7
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-11-22 19:58:41 +0100

    Adds data

--- portfolio/src/app/Layouts/Experience.tsx
+++ portfolio/src/app/Layouts/Experience.tsx
@@ -19,10 +19,13 @@ export default function Experience() {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-grayaccent to-accent group-hover:from-slate-100 group-hover:via-lightpurpleaccent group-hover:to-purpleaccent opacity-70 group-hover:outline group-hover:outline-2 group-hover:outline-secondary rounded-lg transition-colors duration-700"></div>
         <div className="py-2 mx-2 text-sm font-semibold text-justify uppercase relative z-10">
           <div className="flex justify-between">
-            <p className="p-2 text-lg">data</p>
-            <p className="p-2 text-lg">gdzie</p>
+            <p className="p-2 text-lg">Self-directed learning</p>
+            <p className="p-2 text-lg">07.2022 - Present</p>
           </div>
-          <p className="pl-2 font-medium">co robilem</p>
+          <ul className="font-medium list-disc pl-6">
+            <li>Mentored by a senior frontend developer</li>
+            <li>Self-Study Projects</li>
+          </ul>
         </div>
       </motion.div>
 
@@ -34,10 +37,20 @@ export default function Experience() {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-grayaccent to-accent group-hover:from-slate-100 group-hover:via-lightpurpleaccent group-hover:to-purpleaccent opacity-70 group-hover:outline group-hover:outline-2 group-hover:outline-secondary rounded-lg transition-colors duration-700"></div>
         <div className="py-2 mx-2 text-sm font-semibold text-justify uppercase relative z-10">
           <div className="flex justify-between">
-            <p className="p-2 text-lg">data</p>
-            <p className="p-2 text-lg">gdzie</p>
+            <p className="p-2 text-lg">VogelBahn Sp. z o.o. sp. k.</p>
+            <p className="p-2 text-lg">07.2019 - 06.2022</p>
           </div>
-          <p className="pl-2 font-medium">co robilem</p>
+          <p className="pl-2">Road designer assistant</p>
+          <ul className="font-medium list-disc pl-6">
+            <li>
+              Working with clients and city governments to define and finalize
+              projects
+            </li>
+            <li>
+              Leading a designer team - project of an over 5 km long tramline in
+              Sosnowiec, valued at over 80 million PLN
+            </li>
+          </ul>
         </div>
       </motion.div>
 
@@ -49,10 +62,10 @@ export default function Experience() {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-grayaccent to-accent group-hover:from-slate-100 group-hover:via-lightpurpleaccent group-hover:to-purpleaccent opacity-70 group-hover:outline group-hover:outline-2 group-hover:outline-secondary rounded-lg transition-colors duration-700"></div>
         <div className="py-2 mx-2 text-sm font-semibold text-justify uppercase relative z-10">
           <div className="flex justify-between">
-            <p className="p-2 text-lg">data</p>
-            <p className="p-2 text-lg">gdzie</p>
+            <p className="p-2 text-lg">Silesian University of Technology</p>
+            <p className="p-2 text-lg">10.2014 - 05.2019</p>
           </div>
-          <p className="pl-2 font-medium">co robilem</p>
+          <p className="pl-2">Master of Science in Civil Engineering</p>
         </div>
       </motion.div>
     </div>
